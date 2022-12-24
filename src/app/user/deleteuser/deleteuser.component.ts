a1e8553dce53858b293d51c5a7d4d21d5d323182
Yannri Zenner <zenner88@gmail.com>
2022-12-24 20:35:35 +0700

    del

--- src/app/user/deleteuser/deleteuser.component.ts
+++ src/app/user/deleteuser/deleteuser.component.ts
@@ -29,14 +29,18 @@ export class DeleteuserComponent implements OnInit {
   }
 
   reqReset(){
-    let apiBanner = 'https://api-devs.papaden.org/auth/resetpassword/';
+    let apiBanner = 'https://api-devs.papaden.org/deleterequest/';
     console.log(apiBanner);
-    this.httpClient.get<any>(apiBanner+this.emailGet).subscribe({
+    let body = {
+      "status" : "requested",
+      "email"  : this.emailGet
+    }
+    this.httpClient.post<any>(apiBanner,body).subscribe({
       next: data => {
       const code = data.statusCode;
       console.log(data);
       if (data.statusCode == 200){
-        this.message = "Permintaan reset password berhasil, silahkan periksa Inbox di Email : "+this.emailGet
+        this.message = "Permintaan Delete Account berhasil, silahkan periksa Inbox di Email : "+this.emailGet
         // this._snackBar.open(this.message, "OK", {duration: 5000})
         this.isShowForm = true;
         this.isShowS = false;
@@ -47,7 +51,7 @@ export class DeleteuserComponent implements OnInit {
       let errorMessage = error.message;
       // console.error('There was an error!', error);
       if (error.error.statusCode == 500 ){
-        this.message = "Permintaan reset password gagal, pastikan Email yang Anda input benar : "+this.emailGet
+        this.message = "Permintaan Delete Account gagal, pastikan Email yang Anda input benar : "+this.emailGet
         this._snackBar.open(this.message, "OK", {duration: 5000})
       }
   }
